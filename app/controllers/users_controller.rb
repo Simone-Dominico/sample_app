b7e7302c3e266799045ad4481b9d45826b808756
Simone Dominico <simone.dominico@gmail.com>
2013-10-22 13:00:16 -0200

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+	before_action :signed_in_user,
+	only: [:index, :edit, :update, :destroy, :following, :followers]
 	before_action :correct_user,   only: [:edit, :update]
 	before_action :admin_user,     only: :destroy
 	def new
@@ -37,12 +38,26 @@ class UsersController < ApplicationController
 		@users = User.paginate(page: params[:page])
 	end
 
-	 def destroy
-    	User.find(params[:id]).destroy
-    	flash[:success] = "User deleted."
-    	redirect_to users_url
-    end
-    
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User deleted."
+		redirect_to users_url
+	end
+
+	def following
+		@title = "Following"
+		@user = User.find(params[:id])
+		@users = @user.followed_users.paginate(page: params[:page])
+		render 'show_follow'
+	end
+
+	def followers
+		@title = "Followers"
+		@user = User.find(params[:id])
+		@users = @user.followers.paginate(page: params[:page])
+		render 'show_follow'
+	end
+	
 	private
 
 	def user_params
@@ -64,6 +79,6 @@ class UsersController < ApplicationController
     end
 
     def admin_user
-      redirect_to(root_url) unless current_user.admin?
+    	redirect_to(root_url) unless current_user.admin?
     end
 end
